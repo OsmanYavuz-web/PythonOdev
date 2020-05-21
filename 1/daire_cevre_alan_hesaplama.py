4074a53565a72995e15ea6f3d078bae08c9dd839
Osman Yavuz <omnyvz.yazilim@gmail.com>
2020-05-21 20:34:54 +0000

    Add

---
+++ 1/daire_cevre_alan_hesaplama.py
@@ -0,0 +1,14 @@
+while True:
+  # Kullanıcı veri girişi
+  daire_yari_cap = input("Yarıçapı Girin: ") 
+ 
+  # Dairenin çevresini hesaplama
+  daire_cevre = (2 * 3.14 * float(daire_yari_cap))
+  
+  # Dairenin alanını hesaplama
+  daire_alan = 3.14 * float(daire_yari_cap) * float(daire_yari_cap)
+
+  # Ekrana yazdırma
+  print("Dairenin Alanı : ", daire_alan )
+  print("Dairenin Çevresi : ",daire_cevre)
+  print("\n")
